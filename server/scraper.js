77fffcb3982c1fa9323a21ec32a4121d5399a262
mikeg <mikeg@steelhouse.com>
2017-11-24 17:55:45 -0800

    gv scrape wip save

--- server/scraper.js
+++ server/scraper.js
@@ -5,24 +5,35 @@ const phantom = require('phantom');
 
 const scrapeController = {
     getData: (req, res) => {
+        console.log('getData');
         return new Promise((resolve, reject) => {
+            console.log('PROMISE');
             const output = [];
-            async function phantomCherio() {
+            async function goldenVoice() {
+                console.log('goldenVoice');
                 const instance = await phantom.create();
                 const page = await instance.createPage();
     
-                await page.on('onResourceRequested', requestData => {
+                await page.on('onResourceRequested', function(requestData) {
                     console.info('Requesting', requestData.url);
                 });
     
-                const status = await page.open('https://losangeles.ohmyrockness.com/shows?all=true');
+                const status = await page.open('http://www.goldenvoice.com/shows/');
+                console.log('status:  ', status);
                 const content = await page.property('content');
-    
-                let $ = cheerio.load('content');
-                console.log('$CHEERIO:  ', $);
+
+                let $ = cheerio.load(content);
+
+                $('.in').map((idx, elem) => {
+                    const listing = {};
+                    listing.headliner = $(elem).find('.show-info').find('h1').find('a').text();
+                    output.push(listing);
+                });
+
                 await instance.exit();
             };
-            phantomCherio().then(() => {
+            goldenVoice().then(() => {
+                console.log('goldenVoice().then  ', output)
                 res.send(output);
             });
         });
--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const scrapeController = require('./scraper');
 
 const PORT = 8080;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.status(200).send('Welcome to Hit List');
