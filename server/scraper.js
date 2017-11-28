ea1b459f9e3bc966b43fe4dbd1a466ca57cc7f4f
mikeg <mikeg@steelhouse.com>
2017-11-28 12:40:45 -0800

    eslint / standardize date / refactor spaceland

--- server/scraper.js
+++ server/scraper.js
@@ -10,124 +10,104 @@ const moment = require('moment');
 // TODO: catch errors
 
 const scrapeController = {
-    getData: (req, res) => {
-        return new Promise((resolve, reject) => {
+  getData: (req, res) => {
+    return new Promise((resolve, reject) => {
+      const output = [];
 
-            const output = [];
+      async function goldenVoice() {
+        const instance = await phantom.create();
+        const page = await instance.createPage();
 
-            async function goldenVoice() {
-                // TODO: click .loadmore & scrape
-                const instance = await phantom.create();
-                const page = await instance.createPage();
-    
-                await page.on('onResourceRequested', function(requestData) {
-                    console.info('Requesting', requestData.url);
-                });
-    
-                const status = await page.open('http://www.goldenvoice.com/shows/');
-                const content = await page.property('content');
+        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
 
-                let $ = cheerio.load(content);
+        const status = await page.open('http://www.goldenvoice.com/shows/');
+        const content = await page.property('content');
 
-                $('.in').map((idx, elem) => {
-                    const listing = {};
-                    listing.headliner = $(elem).find('.show-info > h1 > a').text();
-                    listing.support = $(elem).find('.show-info > .support').text();
-                    listing.venue = $(elem).find('.show-info > .venue').text().split(',')[0];
-                    listing.city = $(elem).find('.show-info > .venue').text().split(',')[1];
-                    listing.state = $(elem).find('.show-info > .venue').text().split(',')[2];
-                    listing.date = moment($(elem).find('.show-info > .date').text().split(',')[0]).format('YYYY-MM-DD');
-                    listing.time = $(elem).find('.show-info > .date').text().split(',')[1];
-                    listing.tickets = $(elem).find('.bottomshowpart > .wrapbuttonsshows > .buy-ticketlink').attr('href');
-                    output.push(listing);
-                });
+        let $ = cheerio.load(content);
 
-                await instance.exit();
-            };
-
-            async function liveNation() {
-                // TODO: click .btn-more & scrape
-                // TODO: support 
-                const instance = await phantom.create();
-                const page = await instance.createPage();
-
-                await page.on('onResourceRequested', function (requestData) {
-                    console.info('Requesting', requestData.url);
-                });
-
-                const status = await page.open('https://www.livenation.com/cities/89824/los-angeles-ca');
-                const content = await page.property('content');
-
-                let $ = cheerio.load(content);
-
-                const cityState = $('#city > .name > h1').text().split('Shows in ')[1];
-                const city = cityState.split(',')[0];
-                const state = cityState.split(',')[1].trim();
-
-                $('.ad-slot-count').map((idx, elem) => {
-                    const listing = {};
-                    listing.headliner = $(elem).find('.event-details > h3').text();
-                    listing.support = 'LIVENATION TO DO';
-                    listing.venue = $(elem).find('.event-details > h4 > a > span').text();
-                    listing.city = city;
-                    listing.state = state;
-                    listing.date = moment($(elem).find('.event-details > .event-row-date > .event-date').attr('content')).format('YYYY-MM-DD');
-                    listing.time = $(elem).find('.event-details > .event-row-date > .event-day').text().split('@ ')[1];
-                    listing.tickets = 'https://www.livenation.com' + $(elem).find('.event-row-buy > div > a').attr('href');
-                    output.push(listing);
-                });
-
-                await instance.exit();
-            };
-
-            async function spaceLand() {
-                // TODO: click .btn-more & scrape
-                // TODO: support 
-                const instance = await phantom.create();
-                const page = await instance.createPage();
-
-                await page.on('onResourceRequested', function (requestData) {
-                    console.info('Requesting', requestData.url);
-                });
-
-                const status = await page.open('https://www.spacelandpresents.com/events/');
-                const content = await page.property('content');
-
-                let $ = cheerio.load(content);
-
-                $('.list-view-item').map((idx, elem) => {
-                    // TODO: figure out how to get this date in proper format for sorting
-                    const dateFormat = $(elem).find('.list-view-details > .dates').text().substring(4) + ' 2017';
-
-
-                    const listing = {};
-                    listing.headliner = $(elem).find('.list-view-details > .headliners > a').text();
-                    listing.support = 'SPACELAND TO DO';
-                    listing.venue = $(elem).find('.list-view-details > .venue').text();
-                    listing.city = 'Los Angeles';
-                    listing.state = 'CA';
-                    listing.date = dateFormat;
-                    listing.time = 'SPACELAND TO DO';
-                    listing.tickets = $(elem).find('.ticket-price > h3 > a').attr('href');
-                    output.push(listing);
-                });
-
-                await instance.exit();
-            };
-
-            // goldenVoice().then(() => liveNation().then(() => spaceLand().then(() => {
-            //     const sortedOutput = output.sort((a, b) => {
-            //         const dateA = moment(a.date);
-            //         const dateB = moment(b.date);
-            //         return dateA - dateB;
-            //     });
-            //     res.send(sortedOutput);
-            // })));
-            spaceLand().then(() => {
-                res.send(output);
-            });
+        $('.in').map((idx, elem) => {
+          const listing = {};
+          listing.headliner = $(elem).find('.show-info > h1 > a').text();
+          listing.support = $(elem).find('.show-info > .support').text();
+          listing.venue = $(elem).find('.show-info > .venue').text().split(',')[0];
+          listing.city = $(elem).find('.show-info > .venue').text().split(',')[1];
+          listing.state = $(elem).find('.show-info > .venue').text().split(',')[2].trim();
+          listing.date = moment($(elem).find('.show-info > .date').text().split(',')[0]).format('YYYY-MM-DD');
+          listing.time = $(elem).find('.show-info > .date').text().split(',')[1];
+          listing.tickets = $(elem).find('.bottomshowpart > .wrapbuttonsshows > .buy-ticketlink').attr('href');
+          output.push(listing);
         });
-    }
+
+        await instance.exit();
+      }
+
+      async function liveNation() {
+        const instance = await phantom.create();
+        const page = await instance.createPage();
+
+        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
+
+        const status = await page.open('https://www.livenation.com/cities/89824/los-angeles-ca');
+        const content = await page.property('content');
+
+        let $ = cheerio.load(content);
+
+        const cityState = $('#city > .name > h1').text().split('Shows in ')[1];
+        const city = cityState.split(',')[0];
+        const state = cityState.split(',')[1].trim();
+
+        $('.ad-slot-count').map((idx, elem) => {
+          const listing = {};
+          listing.headliner = $(elem).find('.event-details > h3').text();
+          listing.support = 'LIVENATION TO DO';
+          listing.venue = $(elem).find('.event-details > h4 > a > span').text();
+          listing.city = city;
+          listing.state = state;
+          listing.date = moment($(elem).find('.event-details > .event-row-date > .event-date').attr('content')).format('YYYY-MM-DD');
+          listing.time = $(elem).find('.event-details > .event-row-date > .event-day').text().split('@ ')[1];
+          listing.tickets = 'https://www.livenation.com' + $(elem).find('.event-row-buy > div > a').attr('href');
+          output.push(listing);
+        });
+
+        await instance.exit();
+      }
+
+      async function spaceLand() {
+        const instance = await phantom.create();
+        const page = await instance.createPage();
+
+        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
+
+        const status = await page.open('https://www.spacelandpresents.com/events/');
+        const content = await page.property('content');
+
+        let $ = cheerio.load(content);
+        $('#detail > .detail-view > .list-view > .list-view-item').map((idx, elem) => {
+          const listing = {};
+          listing.headliner = $(elem).find('.list-view-details > .headliners > a').text();
+          listing.support = $(elem).find('.list-view-details > .supports > a').text();
+          listing.venue = $(elem).find('.list-view-details > .venue').text();
+          listing.city = $(elem).find('.list-view-details > .city-state').text().split(',')[0];
+          listing.state = $(elem).find('.list-view-details > .city-state').text().split(',')[1].trim();
+          listing.date = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('YYYY-MM-DD');
+          listing.time = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('h:mm a');
+          listing.tickets = $(elem).find('.ticket-price > h3 > a').attr('href');
+          output.push(listing);
+        });
+
+        await instance.exit();
+      }
+
+      goldenVoice().then(() => liveNation().then(() => spaceLand().then(() => {
+        const sortedOutput = output.sort((a, b) => {
+          const dateA = moment(a.date);
+          const dateB = moment(b.date);
+          return dateA - dateB;
+        });
+        res.send(sortedOutput);
+      })));
+    });
+  },
 };
 
 module.exports = scrapeController;
