164094f0b51ead44aa89198178cd97c9afd4611f
mikeg <mikeg@steelhouse.com>
2017-11-29 09:16:18 -0800

    puppeteer refactor wip save

--- server/scraper.js
+++ server/scraper.js
@@ -2,6 +2,7 @@
 
 const cheerio = require('cheerio');
 const phantom = require('phantom');
+const puppeteer = require('puppeteer');
 const moment = require('moment');
 
 // const TODO = {
@@ -18,93 +19,93 @@ const scrapeController = {
       const output = [];
 
       async function goldenVoice() {
-        const instance = await phantom.create();
-        const page = await instance.createPage();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
-        await page.open('http://www.goldenvoice.com/shows/');
+        // await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
+        await page.goto('http://www.goldenvoice.com/shows/');
+        await page.content();
+        // const content = await page.content();
+        // const $ = cheerio.load(content);
+        console.log('PAGE:  ', page.$('.in'));
+        // await page.$('.in').map((elem) => {
+        //   const listing = {};
+        //   listing.headliner = $(elem).find('.show-info > h1 > a').text();
+        //   listing.support = $(elem).find('.show-info > .support').text();
+        //   listing.venue = $(elem).find('.show-info > .venue').text().split(',')[0];
+        //   listing.city = $(elem).find('.show-info > .venue').text().split(',')[1];
+        //   listing.state = $(elem).find('.show-info > .venue').text().split(',')[2].trim();
+        //   listing.date = moment($(elem).find('.show-info > .date').text().split(',')[0]).format('YYYY-MM-DD');
+        //   listing.time = $(elem).find('.show-info > .date').text().split(',')[1].trim().toLowerCase();
+        //   listing.tickets = $(elem).find('.bottomshowpart > .wrapbuttonsshows > .buy-ticketlink').attr('href');
+        //   output.push(listing);
+        // });
 
-        const content = await page.property('content');
-        const $ = cheerio.load(content);
-
-        $('.in').map((idx, elem) => {
-          const listing = {};
-          listing.headliner = $(elem).find('.show-info > h1 > a').text();
-          listing.support = $(elem).find('.show-info > .support').text();
-          listing.venue = $(elem).find('.show-info > .venue').text().split(',')[0];
-          listing.city = $(elem).find('.show-info > .venue').text().split(',')[1];
-          listing.state = $(elem).find('.show-info > .venue').text().split(',')[2].trim();
-          listing.date = moment($(elem).find('.show-info > .date').text().split(',')[0]).format('YYYY-MM-DD');
-          listing.time = $(elem).find('.show-info > .date').text().split(',')[1].trim().toLowerCase();
-          listing.tickets = $(elem).find('.bottomshowpart > .wrapbuttonsshows > .buy-ticketlink').attr('href');
-          output.push(listing);
-        });
-
-        await instance.exit();
+        await browser.close();
       }
 
-      async function liveNation() {
-        const instance = await phantom.create();
-        const page = await instance.createPage();
+      // async function liveNation() {
+      //   const instance = await phantom.create();
+      //   const page = await instance.createPage();
 
-        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
-        await page.open('https://www.livenation.com/cities/89824/los-angeles-ca');
+      //   await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
+      //   await page.open('https://www.livenation.com/cities/89824/los-angeles-ca');
 
-        const content = await page.property('content');
-        const $ = cheerio.load(content);
+      //   const content = await page.property('content');
+      //   const $ = cheerio.load(content);
 
-        const cityState = $('#city > .name > h1').text().split('Shows in ')[1];
-        const city = cityState.split(',')[0];
-        const state = cityState.split(',')[1].trim();
+      //   const cityState = $('#city > .name > h1').text().split('Shows in ')[1];
+      //   const city = cityState.split(',')[0];
+      //   const state = cityState.split(',')[1].trim();
 
-        const formatTime = (timeStamp) => {
-          let digits = timeStamp.slice(0, -2);
-          const meridiem = timeStamp.slice(-2);
-          if (digits.length === 1) digits += ':00';
-          return `${digits} ${meridiem}`;
-        };
+      //   const formatTime = (timeStamp) => {
+      //     let digits = timeStamp.slice(0, -2);
+      //     const meridiem = timeStamp.slice(-2);
+      //     if (digits.length === 1) digits += ':00';
+      //     return `${digits} ${meridiem}`;
+      //   };
 
-        $('.ad-slot-count').map((idx, elem) => {
-          const listing = {};
-          listing.headliner = $(elem).find('.event-details > h3').text();
-          listing.support = 'Not listed';
-          listing.venue = $(elem).find('.event-details > h4 > a > span').text();
-          listing.city = city;
-          listing.state = state;
-          listing.date = moment($(elem).find('.event-details > .event-row-date > .event-date').attr('content')).format('YYYY-MM-DD');
-          listing.time = formatTime($(elem).find('.event-details > .event-row-date > .event-day').text().split('@ ')[1]);
-          listing.tickets = 'https://www.livenation.com' + $(elem).find('.event-row-buy > div > a').attr('href');
-          output.push(listing);
-        });
+      //   $('.ad-slot-count').map((idx, elem) => {
+      //     const listing = {};
+      //     listing.headliner = $(elem).find('.event-details > h3').text();
+      //     listing.support = 'Not listed';
+      //     listing.venue = $(elem).find('.event-details > h4 > a > span').text();
+      //     listing.city = city;
+      //     listing.state = state;
+      //     listing.date = moment($(elem).find('.event-details > .event-row-date > .event-date').attr('content')).format('YYYY-MM-DD');
+      //     listing.time = formatTime($(elem).find('.event-details > .event-row-date > .event-day').text().split('@ ')[1]);
+      //     listing.tickets = 'https://www.livenation.com' + $(elem).find('.event-row-buy > div > a').attr('href');
+      //     output.push(listing);
+      //   });
 
-        await instance.exit();
-      }
+      //   await instance.exit();
+      // }
 
-      async function spaceLand() {
-        const instance = await phantom.create();
-        const page = await instance.createPage();
+      // async function spaceLand() {
+      //   const instance = await phantom.create();
+      //   const page = await instance.createPage();
 
-        await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
-        await page.open('https://www.spacelandpresents.com/events/');
+      //   await page.on('onResourceRequested', requestData => console.info('Requesting', requestData.url));
+      //   await page.open('https://www.spacelandpresents.com/events/');
 
-        const content = await page.property('content');
-        const $ = cheerio.load(content);
+      //   const content = await page.property('content');
+      //   const $ = cheerio.load(content);
     
-        $('#detail > .detail-view > .list-view > .list-view-item').map((idx, elem) => {
-          const listing = {};
-          listing.headliner = $(elem).find('.list-view-details > .headliners > a').text();
-          listing.support = $(elem).find('.list-view-details > .supports > a').text();
-          listing.venue = $(elem).find('.list-view-details > .venue').text();
-          listing.city = $(elem).find('.list-view-details > .city-state').text().split(',')[0];
-          listing.state = $(elem).find('.list-view-details > .city-state').text().split(',')[1].trim();
-          listing.date = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('YYYY-MM-DD');
-          listing.time = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('h:mm a');
-          listing.tickets = $(elem).find('.ticket-price > h3 > a').attr('href') || 'Free';
-          output.push(listing);
-        });
+      //   $('#detail > .detail-view > .list-view > .list-view-item').map((idx, elem) => {
+      //     const listing = {};
+      //     listing.headliner = $(elem).find('.list-view-details > .headliners > a').text();
+      //     listing.support = $(elem).find('.list-view-details > .supports > a').text();
+      //     listing.venue = $(elem).find('.list-view-details > .venue').text();
+      //     listing.city = $(elem).find('.list-view-details > .city-state').text().split(',')[0];
+      //     listing.state = $(elem).find('.list-view-details > .city-state').text().split(',')[1].trim();
+      //     listing.date = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('YYYY-MM-DD');
+      //     listing.time = moment($(elem).find('.list-view-details > .times > .start > span').attr('title')).format('h:mm a');
+      //     listing.tickets = $(elem).find('.ticket-price > h3 > a').attr('href') || 'Free';
+      //     output.push(listing);
+      //   });
 
-        await instance.exit();
-      }
+      //   await instance.exit();
+      // }
 
       // async function sendSortedData() {
       //   const sortedOutput = output.sort((a, b) => {
@@ -118,7 +119,17 @@ const scrapeController = {
 
       // Promise.all([goldenVoice, liveNation, spaceLand, sendSortedData]);
 
-      goldenVoice().then(() => liveNation().then(() => spaceLand().then(() => {
+      // goldenVoice().then(() => liveNation().then(() => spaceLand().then(() => {
+      //   const sortedOutput = output.sort((a, b) => {
+      //     const dateA = moment(a.date);
+      //     const dateB = moment(b.date);
+      //     return dateA - dateB;
+      //   });
+      //   console.log('Scrape complete');
+      //   res.send(sortedOutput);
+      // })));
+
+      goldenVoice().then(() => {
         const sortedOutput = output.sort((a, b) => {
           const dateA = moment(a.date);
           const dateB = moment(b.date);
@@ -126,7 +137,7 @@ const scrapeController = {
         });
         console.log('Scrape complete');
         res.send(sortedOutput);
-      })));
+      }).catch(err => console.error(err));
     });
   },
 };
